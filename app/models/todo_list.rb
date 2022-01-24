01bfe7edc128c385d9bdc8222cc4cd400f7fcef4
Adrian Grimm <usmcamgrimm@gmail.com>
2022-01-24 05:18:47 -0500

    Define methods for progress bar completion percentage

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,16 @@
 class TodoList < ApplicationRecord
   has_many :todo_items
+
+  def percent_complete
+    return 0 if total_items == 0
+    (100 * completed_items.to_f / total_items).round(1)
+  end
+
+  def completed_items
+    @completed_items ||= todo_items.completed.count
+  end
+
+  def total_items
+    @total_items ||= todo_items.count
+  end
 end
